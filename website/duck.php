e88bc9b92d88603afda035989bd32488d7222c24
worstfrontendcoder <omarsumon1030@gmail.com>
2021-07-31 13:50:37 -0500

    navbar and working links

---
+++ website/duck.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.3/css/all.css" integrity="sha384-SZXxX4whJ79/gErwcOYf+zWLeJdY/qpuqC4cAa9rOGUstPomtqpuNWT9wdPEn2fk" crossorigin="anonymous">
+    <link rel="stylesheet" href="index.css">
+    <title>Duck.io</title>
+</head>
+<body>
+    <nav>
+        <ul>
+            <li><h1><a href="index.php"><b>Duck.io</b><i class="fas fa-feather-alt"></i></a></h1></li>
+            <li><h2><a href="index.php">Home</a></h2></li>
+            <li><h2><a href="about.php">About</a></h2></li>
+            <li><h2><a href="duck.php">Info</a></h2></li>
+        </ul>
+    </nav>
+    <?php echo("hello duck"); ?>
+</body>
+</html>
